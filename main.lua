7ca201b3e44ce014d6d3b9b3a6dacaad7d5e7cd2
ArjunaRus <arjuna_na@mail.ru>
2015-09-17 02:22:23 +0600

    Refactoring of document copying

--- main.lua
+++ main.lua
@@ -119,7 +119,7 @@ box = obj {
 };
 
 turn_screw = obj {
-    verb = 'отвертеть';
+    verb = 'раскрутить';
 
     nam = 'отвертка';
     dsc = '{отвертка}';
@@ -128,7 +128,7 @@ turn_screw = obj {
 } :disable();
 
 thermal_compound = obj {
-    verb = 'намазать';
+    verb = 'намазать термопастой';
     
     nam = 'термопаста';
     dsc = '{термопаста}';
@@ -160,7 +160,7 @@ screen = dlg {
                                                                  'Мою фотографию прокоментировали.'})]]};
         {always = true, 'livejournal.com', 'В ЖЖ ничего нового.'};
         {always = true, 'google.com', code = [[if _needWeather then
-                                                   _src = 'Google'; 
+                                                   screen._src = 'Google'; 
                                                    psub 'weather';
                                                else
                                                    return rndItem({'Я зашел в гугл.',
@@ -170,7 +170,7 @@ screen = dlg {
                                                end]]};
         {always = true, 'yandex.ru', code = [[if _needWeather then
                                                   psub 'weather';
-                                                  _src = 'Yandex';
+                                                  screen._src = 'Yandex';
                                               else
                                                   return rndItem({'Я зашел в яндекс.',
                                                                   'Что-то я забыл чего хотел найти...',
@@ -178,9 +178,9 @@ screen = dlg {
                                               end]]};
         {};
         {tag = 'weather', "Я ввожу в строке поиска ПОГОДА... На экране появляется несколько ссылок."};
-        {'Погода в России. Распечатать','Я пускаю погоду на печать.', [[putWeather(_src, 'Россия'); pret()]]};
-        {'Погода в Москве. Распечатать','Я пускаю погоду на печать.', [[putWeather(_src, 'Москва'); pret()]]};
-        {'Погода в Барнауле. Распечатать','Я пускаю погоду на печать.', [[putWeather(_src, 'Барнаул'); pret()]]};
+        {'Погода в России. Распечатать','Я пускаю погоду на печать.', [[putWeather(screen._src, 'Россия'); pret()]]};
+        {'Погода в Москве. Распечатать','Я пускаю погоду на печать.', [[putWeather(screen._src, 'Москва'); pret()]]};
+        {'Погода в Барнауле. Распечатать','Я пускаю погоду на печать.', [[putWeather(screen._src, 'Барнаул'); pret()]]};
         {always = true, 'Назад', code = [[pret()]]}
     };
     
@@ -190,7 +190,7 @@ screen = dlg {
 weatherPaper = obj {
     _src = nil;
     _place = nil;
-    verb = 'показать';
+    verb = 'завернуть';
 
     nam = 'распечатка погоды';
     dsc = 'В принтере лежит {распечатка погоды}.';
@@ -210,7 +210,7 @@ putWeather = function(src, place)
     return;
 end;
 
-------------------------------------------------------------------------
+--======================================================================
 
 cab6 = room {
     enter = function()
@@ -235,20 +235,27 @@ cab6 = room {
     way = {'lobby_end','main'};
 };
 
-mfp1 = obj {
-    _jamming = true;
-    
+isMfp1Jammed = function()
+    return (exist(jammedPaper, mfpInside) ~= nil);
+end;
+
+mfp1 = obj { 
     nam = 'Sharp AL-1217';
     dsc = 'Рядом со столом стоит старое {МФУ Sharp}.';
     used = function(s, w)
         if w == someDocument then
-            drop(w, s);
+            p('Я поместил ' .. w.nam .. ' внутрь МФУ на стекло сканера.');
+            placef(w, s);
+            inv():del(w);
+        else
+            p(rndItem({w.verb .. 'МФУ? Хм... ну не знаю, не знаю...',
+                        'Можно конечно ' .. w.verb .. 'МФУ, но непонятно что делать потом..'}));
         end;
         return true;
     end;
     act = function(s)
-        if objs(s):srch('jammedPaper') then
-            objs(s):srch('mfpCover'):enable();
+        if isMfp1Jammed() and disabled(mfpInside) then
+            enable(mfpInside);
             return 'Осмотрев МФУ, сбоку я обнаружил крышку.';
         elseif (objs(s):srch(someDocument) ~= nil) then
             walkin('mfuPanel');
@@ -257,75 +264,94 @@ mfp1 = obj {
             return 'Старая МФУ-шка, уже снятая с производства, модель Sharp AL-1217.';
         end;
     end;
-    obj = {
-        obj {
-            _opened = false;
-            nam = 'mfpCover';
-            dsc = function()
-                if _opened then
-                    return '{Крышка} МФУ открыта.';
-                else
-                    return 'Сбоку у МФУ есть {крышка}.';
-                end;
-            end;
-            
-        }:disable();
-        
-        obj {
-            nam = 'jammedPaper';
-            dsc = 'В механизме виден смятый {лист бумаги}.';
-            act = function(s)
-                objs():del(s);
-                return 'Я вытащил помятый лист из механизма';
-            end,
-            
-        }:disable();
-    };
+    obj = {'mfpInside'};
 };
 
+mfpInside = obj {
+    _opened = false;
+    _fixed = false;
+    
+    nam = 'mfpInside';
+    dsc = function(s)
+        if s._opened then
+            return '{Крышка} МФУ открыта.';
+        else
+            return '{Крышка} МФУ закрыта.';
+        end;
+    end;
+    act = function(s)
+        if s._opened then
+            s._opened = false;
+            return 'Я захлопнул крышку';
+        else
+            return 'Крышка закрыта';
+        end;
+    end;
+    obj = {
+        obj {
+            nam = 'lock';
+            dsc = function(s)
+                if not mfpInside._opened then
+                    return 'Она защелкивается на {защелку}.';
+                end;
+            end;
+            act = function(s)
+                mfpInside._opened = true;
+                return 'Я нажал на защелку, крышка открылась.';
+            end;
+        };
+        
+        obj {
+            nam = 'screw';
+            dsc = function(s)
+                if mfpInside._opened then
+                    return '^Внутри МФУ выделяется один {болт}, который держит механизм.';
+                end;
+            end;
+            act = function(s)
+                if mfpInside._fixed then
+                    return 'Отрегулированный болт, может быть теперь бумага не будет зажевываться.';
+                else 
+                    return 'Болт держит механизм прохода бумаги. Его бы подрегулировать..';
+                end;
+            end;
+            used = function(s, w)
+                if w == turn_screw then
+                    if (mfpInside._fixed) then
+                        return 'Я уже отрегулировал болт, лучше к нему теперь не лезть.';
+                    end;
+                    
+                    mfpInside._fixed = true;
+                    return 'Я подрегулировал болт, кажется теперь не должно зажевывать.';
+                else 
+                    return 'Предлагаете ' .. w.verb .. ' болт? Мдя... его надо подрегулировать, а не ' .. w.verb .. '!';
+                end;
+            end;
+        };
+    };
+    
+}:disable();
+
+jammedPaper = obj {
+    nam = 'jammedPaper';
+    dsc = function(s)
+        if mfpInside._opened then
+            return '^В механизме виден смятый {лист бумаги}.';
+        end;
+    end;
+    act = function(s)
+        remove(s, mfpInside);
+        return 'Я вытащил помятый лист из механизма';
+    end;
+};
+
+-- TODO: change this dialog to room, because too much code inside strings
 mfuPanel = dlg {
+    _copyMode = false;
     hideinv = true;
     
     nam = 'Ксерокопирование';
     dsc = 'На лицевой панели МФУ располагаются органы управления';
-	
-    _copyMode= false;
-	--[[
-	act = function(s,w)
-		if w==2 then
-			if mfucover._opened then
-				return 'Крышка МФУ открыта, в этом состоянии он не будет работать';
-			else
-				if mfu1._paperinside then
-					return 'Кажется внутри зажевана бумага, сначала надо ее вытащить.';
-				else
-					if s._copymode then
-						if mfu1._fixed then
-							if game._makecopyquest then
-								game._quests=game._quests+1;
-								game._makecopyquest = false;
-								mfu1.fixed = false;
-								back();
-								inv():del('sheetpaper');
-								copyhuman:disable();
-								game._MFUvoid = false;
-								return 'Ну все, кажется копия успешно снялась, УРА!';
-							else 
-								back();
-								inv():del('sheetpaper');
-								return 'Копия успешно снялась!';
-							end;
-						else 
-							mfu1._paperinside = true;
-							return 'Ну вот, МФУ зажевал бумагу. Он очень старый я ведь предупреждал.';
-						end;
-					else
-						return 'МФУ стоит в режиме сканирования. В этом режиме копии не снимаются.';
-					end;
-				end;
-			end;
-		end;
-	end,]]
 	phr = {
 		{always = true, 'Кнопка переключения режима.', 'Режим работы:', 
         [[
@@ -335,16 +361,34 @@ mfuPanel = dlg {
             else
                 p 'сканирование'
             end;
-        ]]},
-		{always = true, 'Кнопка копирования.', 'Z нажал кнопку копирования', [[objs('mfp1'):srch('jammedPaper'):enable()]]},
-		{always = true, 'Кнопки настройки изображения.', 'Я настроил изображение получше. Хотя, вроде, и так было норм.'},
+        ]]};
+		{always = true, 'Кнопка копирования.', 'Я нажал кнопку копирования...^', 
+        [[
+            if not _copyMode then
+                p 'МФУ стоит в режиме сканирования. В этом режиме копии не снимаются.';
+            elseif mfpInside._opened then
+                p 'Крышка МФУ открыта, в этом состоянии он не будет работать';
+            elseif  isMfp1Jammed() then
+                p 'Кажется внутри зажевана бумага, сначала надо ее вытащить.';
+            elseif not mfpInside._fixed then
+                place(jammedPaper, mfpInside);
+                p 'Ну вот, МФУ зажевал бумагу. Он очень старый я ведь предупреждал!';
+                back();
+            else
+                objs(mfp1):del(someDocument);
+                achivs._value = achivs._value + 1;
+                p 'Ну все, кажется копия успешно снялась, УРА!';
+                back();
+            end;
+        ]]};
+		{always = true, 'Кнопки настройки изображения.', 'Я настроил изображение получше. Хотя, вроде, и так было норм.'};
 	};
     
     way = {'cab6'};
 };
 
 someDocument = obj {
-    verb = 'показать';
+    verb = 'завернуть';
     
 	nam = 'какой-то документ';
 	inv = 'Этот документ мне дали чтобы снять с него копию.';
@@ -352,7 +396,7 @@ someDocument = obj {
     tak = 'Я вытащил документ из сканера.';
 };
 
-------------------------------------------------------------------------
+--======================================================================
 
 lobby_end = room {
     enter = function(s,f)
